a2408d4a011f473e00fcbb549eac379a4eb662ef
gaaat <ga.gatti98@gmail.com>
2020-08-04 14:49:11 +0200

    first functionalities implemented

--- bot.py
+++ bot.py
@@ -5,8 +5,13 @@ Author: liuhh02 https://medium.com/@liuhh02
 """
 
 import logging
-from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
+from telegram.ext import (Updater, CommandHandler, MessageHandler, Filters,
+                          ConversationHandler)
+from telegram import (ReplyKeyboardMarkup, ReplyKeyboardRemove)
 import os
+
+from fetcher import Fetcher
+
 PORT = int(os.environ.get('PORT', 5000))
 
 # Enable logging
@@ -16,23 +21,97 @@ logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s
 logger = logging.getLogger(__name__)
 TOKEN = '1390945914:AAFlBPy0JbmtRzXg7ob2T3TRKoDaiVgpwpI'
 
+TITLES, FETCH = range(2)
+
 # Define a few command handlers. These usually take the two arguments update and
 # context. Error handlers also receive the raised TelegramError object in error.
+def instantiateFetcher(update, context):
+    if "fetcher" not in context.user_data.keys():
+        user = update.message.from_user
+        context.user_data["fetcher"] = Fetcher(user.id)
+
+def sendTitleMessage(update, context, title, lastn, lasturl):
+    try:
+        update.message.reply_markdown(f'*=== {title} ===*\n'
+                                      f'Ultimo capitolo: *{lastn}*\n'
+                                      f'*LINK:* {lasturl}')
+    except:
+        update.message.reply_html(f'<b>=== {title} ===</b>\n'
+                                  f'Ultimo capitolo: <b>{lastn}</b>\n'
+                                  f'<b>LINK:</b> {lasturl}')
+
 def start(update, context):
     """Send a message when the command /start is issued."""
-    update.message.reply_text('Hi!')
+    user = update.message.from_user
+    instantiateFetcher(update, context)
+    reply_keyboard = [['/add manga'], ['/check for updates']]
+    update.message.reply_text(f'Benvenuto {user.first_name}!\n'
+                               'Cosa vuoi fare?',
+                               reply_markup=ReplyKeyboardMarkup(reply_keyboard, one_time_keyboard=True))
 
 def help(update, context):
     """Send a message when the command /help is issued."""
-    update.message.reply_text('Help!')
+    update.message.reply_text('Prima cosa modificata fra')
 
-def echo(update, context):
-    """Echo the user message."""
-    update.message.reply_text(update.message.text)
+def add(update, context):
+    instantiateFetcher(update, context)
+    update.message.reply_text(f'Inserisci il titolo da ricercare')
+    return TITLES
+
+def checkAll(update, context):
+    instantiateFetcher(update, context)
+    res = context.user_data["fetcher"].checkRelease()
+    if res == []:
+        emptyListMessage(update, context)
+    else:
+        for t in res:
+            sendTitleMessage(update, context, *t)
+
+def listAll(update, context):
+    instantiateFetcher(update, context)
+    titles = context.user_data["fetcher"].listMangaTitles()
+    if titles == []:
+        emptyListMessage(update, context)
+    else:
+        message = ""
+        for t in titles:
+            message += f"+ *{t}*\n"
+        update.message.reply_markdown(message)
+def emptyListMessage(update, context):
+    message = "No titles found in your list!\nStart adding with /add"
+    update.message.reply_markdown(message)
+
+def getTitles(update, context):
+    title = update.message.text
+    options = context.user_data["fetcher"].fetchManga(title)
+    if options == {}:
+        update.message.reply_text("Not found :(")
+        return ConversationHandler.END
+    else:
+        context.user_data["title_list"] = options
+        reply_keyboard = [[k] for k in options.keys()]
+        reply_keyboard.append(["/cancel"])
+        update.message.reply_text(f'Seleziona il titolo dalla lista\n',
+                               reply_markup=ReplyKeyboardMarkup(reply_keyboard, one_time_keyboard=True))
+        return FETCH
+
+def fetchLatest(update, context):
+    title = update.message.text
+    lastn, lasturl = context.user_data["fetcher"].selectMangaAddAndFetch(context.user_data["title_list"], title)
+    del context.user_data["title_list"]
+    sendTitleMessage(update, context, title, lastn, lasturl)
+    return ConversationHandler.END
 
 def error(update, context):
     """Log Errors caused by Updates."""
     logger.warning('Update "%s" caused error "%s"', update, context.error)
+
+def cancel(update, context):
+    user = update.message.from_user
+    logger.info("User %s canceled the action.", user.first_name)
+    update.message.reply_text('Operazione annullata')
+
+    return ConversationHandler.END
 
 def main():
     """Start the bot."""
@@ -45,11 +124,20 @@ def main():
     dp = updater.dispatcher
 
     # on different commands - answer in Telegram
-    dp.add_handler(CommandHandler("start", start))
-    dp.add_handler(CommandHandler("help", help))
+    conv_handler = ConversationHandler(
+        entry_points=[CommandHandler('add', add)],
 
-    # on noncommand i.e message - echo the message on Telegram
-    dp.add_handler(MessageHandler(Filters.text, echo))
+        states={
+            TITLES: [MessageHandler(Filters.text & ~Filters.command, getTitles)],
+            FETCH: [MessageHandler(Filters.text & ~Filters.command, fetchLatest)],
+        },
+
+        fallbacks=[CommandHandler("cancel", cancel)]
+    )
+    dp.add_handler(CommandHandler("start", start))
+    dp.add_handler(CommandHandler("check", checkAll))
+    dp.add_handler(CommandHandler("list", listAll))
+    dp.add_handler(conv_handler)
 
     # log all errors
     dp.add_error_handler(error)
@@ -58,7 +146,7 @@ def main():
     updater.start_webhook(listen="0.0.0.0",
                           port=int(PORT),
                           url_path=TOKEN)
-    updater.bot.setWebhook('***REMOVED***' + TOKEN)
+    updater.bot.setWebhook('https://7190e44115ad.ngrok.io/' + TOKEN)
 
     # Run the bot until you press Ctrl-C or the process receives SIGINT,
     # SIGTERM or SIGABRT. This should be used most of the time, since
--- user.py
+++ user.py
@@ -1,15 +1,35 @@
+import os
+import redis
 import json
 
 class User():
-    def __init__(self):
-        self.status = {}
+    def __init__(self, username="test"):
+        #redis_url = "redis://redis-13589.c78.eu-west-1-2.ec2.cloud.redislabs.com:13589"
+        redis_url = "redis://localhost:6379"
+        self.redis = redis.from_url(redis_url)
+        self.username = username
+        self.redisGetStatus()
+    
+    def redisGetStatus(self):
+        data = self.redis.get(self.username)
+        if data == None:
+            self.status = {}
+        else:
+            self.status = json.loads(data)
+    
+    def redisSetStatus(self):
+        data = json.dumps(self.status)
+        self.redis.set(self.username, data)
     
     # todo improve data structures ---> maybe add "manga" class
     def addManga(self, title, url, latest_chapter, latest_url):
-        self.status[title] = {}
-        self.status[title]["url"] = url
-        self.status[title]["latestn"] = latest_chapter
-        self.status[title]["latesturl"] = latest_url
+        if title not in self.status.keys():
+            self.status[title] = {}
+            self.status[title]["url"] = url
+            self.status[title]["latestn"] = latest_chapter
+            self.status[title]["latesturl"] = latest_url
+            self.redisSetStatus()
+        
 
     def getLatest(self, title):
         return self.status[title]["latestn"]
@@ -17,12 +37,16 @@ class User():
     def updateLatest(self, title, n, url):
         self.status[title]["latestn"] = n
         self.status[title]["latesturl"] = url
+        self.redisSetStatus()
     
     def getTitlesAndUrls(self):
         ret = {}
         for title in self.status.keys():
             ret[title] = self.status[title]["url"]
         return ret
+
+    def getTitles(self):
+        return list(self.status.keys())
 
     def getUrlFromName(self, title):
         return self.status[title]["url"]
