4464dc7f59a13975a4eb56adc610ee65fe264861
gaaat <ga.gatti98@gmail.com>
2022-11-12 00:40:07 +0100

    small fix

--- bot.py
+++ bot.py
@@ -477,9 +477,12 @@ def main():
     # Start the Bot
     if CERT_PATH != None and CERT_KEY_PATH != None:
         updater.start_webhook(listen="0.0.0.0", port=PORT, url_path=TOKEN, cert=CERT_PATH, key=CERT_KEY_PATH)
+        logger.info('Webhook started WITH certificate!')
     else:
         updater.start_webhook(listen="0.0.0.0", port=PORT, url_path=TOKEN)
-    updater.bot.setWebhook(WEBHOOK_URL + TOKEN)
+        logger.info('Webhook started WITHOUT certificate!')
+
+    updater.bot.setWebhook("https://" + WEBHOOK_URL + ":" + PORT + "/" + TOKEN)
 
     startupRoutine(updater)
     updater.idle()
