60acf2033293e6a98bbed06b139e4ca40234d02d
gaaat <ga.gatti98@gmail.com>
2022-11-12 02:07:16 +0100

    small fix

--- bot.py
+++ bot.py
@@ -476,16 +476,16 @@ def main():
 
     # Start the Bot
     if CERT_PATH != None and CERT_KEY_PATH != None:
-        updater.start_webhook(port=PORT, url_path=TOKEN, webhook_url=f"https://{WEBHOOK_URL}:{str(PORT)}/{TOKEN}", cert=CERT_PATH, key=CERT_KEY_PATH)
+        updater.start_webhook(listen="0.0.0.0", port=PORT, url_path=TOKEN, webhook_url=f"https://{WEBHOOK_URL}:{str(PORT)}/{TOKEN}", cert=CERT_PATH, key=CERT_KEY_PATH)
         logger.info('Webhook started WITH certificate!')
+        # ugly ass workaround to set webhook url because certifcates fuck things up
+        os.system(f' sleep 3 && curl -F "url=https://www.***REMOVED***:8443/***REMOVED***" https://api.telegram.org/bot***REMOVED***/setWebhook &> /dev/null')
+        logger.info('cURL workaround done!')
     else:
-        updater.start_webhook(port=PORT, url_path=TOKEN, webhook_url=f"https://{WEBHOOK_URL}:{str(PORT)}/{TOKEN}")
+        updater.start_webhook(listen="0.0.0.0", port=PORT, url_path=TOKEN, webhook_url=f"https://{WEBHOOK_URL}:{str(PORT)}/{TOKEN}")
         logger.info('Webhook started WITHOUT certificate!')
 
     # updater.bot.setWebhook("https://" + WEBHOOK_URL + ":" + str(PORT) + "/" + TOKEN)
-    # ugly ass workaround to set webhook url because certifcates fuck things up
-    os.system(f' sleep 3 && curl -F "url=https://www.***REMOVED***:8443/***REMOVED***" https://api.telegram.org/bot***REMOVED***/setWebhook &> /dev/null')
-    logger.info('cURL workaround done!')
 
     startupRoutine(updater)
     updater.idle()
