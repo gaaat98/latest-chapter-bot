98b96c7fa68665bfcc2e1aa280a6cf3f25b99789
gaaat <ga.gatti98@gmail.com>
2022-11-12 01:57:39 +0100

    small fix

--- bot.py
+++ bot.py
@@ -476,13 +476,15 @@ def main():
 
     # Start the Bot
     if CERT_PATH != None and CERT_KEY_PATH != None:
-        updater.start_webhook(port=PORT, url_path=TOKEN, cert=CERT_PATH, key=CERT_KEY_PATH)
+        updater.start_webhook(port=PORT, url_path=TOKEN, webhook_url=f"https://{WEBHOOK_URL}:{str(PORT)}/{TOKEN}", cert=CERT_PATH, key=CERT_KEY_PATH)
         logger.info('Webhook started WITH certificate!')
     else:
-        updater.start_webhook(port=PORT, url_path=TOKEN)
+        updater.start_webhook(port=PORT, url_path=TOKEN, webhook_url=f"https://{WEBHOOK_URL}:{str(PORT)}/{TOKEN}")
         logger.info('Webhook started WITHOUT certificate!')
 
-    updater.bot.setWebhook("https://" + WEBHOOK_URL + ":" + str(PORT) + "/" + TOKEN)
+    # updater.bot.setWebhook("https://" + WEBHOOK_URL + ":" + str(PORT) + "/" + TOKEN)
+    # ugly ass workaround to set webhook url because certifcates fuck things up
+    os.system(f'curl -F "url=https://{WEBHOOK_URL}:{str(PORT)}/{TOKEN}" https://api.telegram.org/bot{TOKEN}/setWebhook')
 
     startupRoutine(updater)
     updater.idle()
